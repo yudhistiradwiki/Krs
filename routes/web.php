31cc95ed6908ea4f01f539f9545f4c4339cfc077
Muhammad Dwiki Yudhistira <yudhistiradwiki17@gmail.com>
2022-01-28 11:30:27 +0700

    doneeee

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use App\Http\Controllers\KhsController;
 */
 
 //login
+Route::get('/', [LoginController::class, 'index']) -> name('login') -> middleware('guest');
 Route::get('/login', [LoginController::class, 'index']) -> name('login') -> middleware('guest');
 Route::post('/login', [LoginController::class, 'auth']);
 Route::get('/logout', [LoginController::class, 'logout']);
@@ -44,83 +45,83 @@ Route::get('/lecture', function () {
 
 
 //mahasiswa
-Route::get('/mahasiswa', [MahasiswaController::class, 'index']) -> middleware('auth:user,mahasiswa,dosen');
-Route::get('/mahasiswa/insert', [MahasiswaController::class, 'tambah']) -> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/mahasiswa/save', [MahasiswaController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/mahasiswa/update/{nim}', [MahasiswaController::class, 'edit'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/mahasiswa/updated/{nim}', [MahasiswaController::class, 'update'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/mahasiswa/delete/{nim}', [MahasiswaController::class, 'delete'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/mahasiswa/detail/{nim}', [MahasiswaController::class, 'detail'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/mahasiswa/cari', [MahasiswaController::class, 'cari'])-> middleware('auth:user,mahasiswa,dosen');;
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']) -> middleware('auth:user');
+Route::get('/mahasiswa/insert', [MahasiswaController::class, 'tambah']) -> middleware('auth:user');;
+Route::post('/mahasiswa/save', [MahasiswaController::class, 'simpan'])-> middleware('auth:user');;
+Route::get('/mahasiswa/update/{nim}', [MahasiswaController::class, 'edit'])-> middleware('auth:user');;
+Route::post('/mahasiswa/updated/{nim}', [MahasiswaController::class, 'update'])-> middleware('auth:user');;
+Route::get('/mahasiswa/delete/{nim}', [MahasiswaController::class, 'delete'])-> middleware('auth:user');;
+Route::get('/mahasiswa/detail/{nim}', [MahasiswaController::class, 'detail'])-> middleware('auth:user');;
+Route::get('/mahasiswa/cari', [MahasiswaController::class, 'cari'])-> middleware('auth:user');;
 
 
 //prodi
-Route::get('/prodi', [ProdiController::class, 'index'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/prodi/insert', [ProdiController::class, 'tambah'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/prodi/save', [ProdiController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/prodi/update/{nim}', [ProdiController::class, 'edit'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/prodi/updated/{nim}', [ProdiController::class, 'update'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/prodi/delete/{nim}', [ProdiController::class, 'delete'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/prodi/cari', [ProdiController::class, 'cari'])-> middleware('auth:user,mahasiswa,dosen');;
+Route::get('/prodi', [ProdiController::class, 'index'])-> middleware('auth:user');;
+Route::get('/prodi/insert', [ProdiController::class, 'tambah'])-> middleware('auth:user');;
+Route::post('/prodi/save', [ProdiController::class, 'simpan'])-> middleware('auth:user');;
+Route::get('/prodi/update/{nim}', [ProdiController::class, 'edit'])-> middleware('auth:user');;
+Route::post('/prodi/updated/{nim}', [ProdiController::class, 'update'])-> middleware('auth:user');;
+Route::get('/prodi/delete/{nim}', [ProdiController::class, 'delete'])-> middleware('auth:user');;
+Route::get('/prodi/cari', [ProdiController::class, 'cari'])-> middleware('auth:user');;
 
 
 //dosen
-Route::get('/dosen', [DosenController::class, 'index'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/dosen/insert', [DosenController::class, 'tambah'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/dosen/save', [DosenController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/dosen/update/{nim}', [DosenController::class, 'edit'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/dosen/updated/{nim}', [DosenController::class, 'update'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/dosen/delete/{nim}', [DosenController::class, 'delete'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/dosen/detail/{nim}', [DosenController::class, 'detail'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/dosen/cari', [DosenController::class, 'cari'])-> middleware('auth:user,mahasiswa,dosen');;
+Route::get('/dosen', [DosenController::class, 'index'])-> middleware('auth:user');;
+Route::get('/dosen/insert', [DosenController::class, 'tambah'])-> middleware('auth:user');;
+Route::post('/dosen/save', [DosenController::class, 'simpan'])-> middleware('auth:user');;
+Route::get('/dosen/update/{nim}', [DosenController::class, 'edit'])-> middleware('auth:user');;
+Route::post('/dosen/updated/{nim}', [DosenController::class, 'update'])-> middleware('auth:user');;
+Route::get('/dosen/delete/{nim}', [DosenController::class, 'delete'])-> middleware('auth:user');;
+Route::get('/dosen/detail/{nim}', [DosenController::class, 'detail'])-> middleware('auth:user');;
+Route::get('/dosen/cari', [DosenController::class, 'cari'])-> middleware('auth:user');;
 
 
 //matkul
-Route::get('/matkul', [MatkulController::class, 'index'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/matkul/insert', [MatkulController::class, 'tambah'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/matkul/save', [MatkulController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/matkul/update/{nim}', [MatkulController::class, 'edit'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/matkul/updated/{nim}', [MatkulController::class, 'update'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/matkul/delete/{nim}', [MatkulController::class, 'delete'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/matkul/detail/{nim}', [MatkulController::class, 'detail'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/matkul/cari', [MatkulController::class, 'cari'])-> middleware('auth:user,mahasiswa,dosen');;
+Route::get('/matkul', [MatkulController::class, 'index'])-> middleware('auth:user');;
+Route::get('/matkul/insert', [MatkulController::class, 'tambah'])-> middleware('auth:user');;
+Route::post('/matkul/save', [MatkulController::class, 'simpan'])-> middleware('auth:user');;
+Route::get('/matkul/update/{nim}', [MatkulController::class, 'edit'])-> middleware('auth:user');;
+Route::post('/matkul/updated/{nim}', [MatkulController::class, 'update'])-> middleware('auth:user');;
+Route::get('/matkul/delete/{nim}', [MatkulController::class, 'delete'])-> middleware('auth:user');;
+Route::get('/matkul/detail/{nim}', [MatkulController::class, 'detail'])-> middleware('auth:user');;
+Route::get('/matkul/cari', [MatkulController::class, 'cari'])-> middleware('auth:user');;
 
 
 //tahun akademik
-Route::get('/tahunakad', [TahunAkadController::class, 'index'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/tahunakad/insert', [TahunAkadController::class, 'tambah'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/tahunakad/save', [TahunAkadController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/tahunakad/update/{nim}', [TahunAkadController::class, 'edit'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/tahunakad/updated/{nim}', [TahunAkadController::class, 'update'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/tahunakad/delete/{nim}', [TahunAkadController::class, 'delete'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/tahunakad/cari', [TahunAkadController::class, 'cari'])-> middleware('auth:user,mahasiswa,dosen');;
+Route::get('/tahunakad', [TahunAkadController::class, 'index'])-> middleware('auth:user');;
+Route::get('/tahunakad/insert', [TahunAkadController::class, 'tambah'])-> middleware('auth:user');;
+Route::post('/tahunakad/save', [TahunAkadController::class, 'simpan'])-> middleware('auth:user');;
+Route::get('/tahunakad/update/{nim}', [TahunAkadController::class, 'edit'])-> middleware('auth:user');;
+Route::post('/tahunakad/updated/{nim}', [TahunAkadController::class, 'update'])-> middleware('auth:user');;
+Route::get('/tahunakad/delete/{nim}', [TahunAkadController::class, 'delete'])-> middleware('auth:user');;
+Route::get('/tahunakad/cari', [TahunAkadController::class, 'cari'])-> middleware('auth:user');;
 
 //krs
-Route::get('/krs/{nim}/{id_thn_akad}', [KrsController::class, 'index'])-> middleware('auth:user,mahasiswa,dosen');
+Route::get('/krs/{nim}/{id_thn_akad}', [KrsController::class, 'index'])-> middleware('auth:mahasiswa');
 Route::get('/krs/view', [KrsController::class, 'datamatkul']);
-Route::get('/krs/insert', [DosenController::class, 'tambah'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::post('/krs/save', [KrsController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/krs/cari', [KrsController::class, 'cari'])-> middleware('auth:user,mahasiswa,dosen');;
-Route::get('/krs/print/{nim}/{id_thn_akad}', [KrsController::class, 'generatePDF']);
+Route::get('/krs/insert', [DosenController::class, 'tambah'])-> middleware('auth:mahasiswa');;
+Route::post('/krs/save', [KrsController::class, 'simpan'])-> middleware('auth:mahasiswa');;
+Route::get('/krs/cari', [KrsController::class, 'cari'])-> middleware('auth:mahasiswa');;
+Route::get('/krs/print/{nim}/{id_thn_akad}', [KrsController::class, 'generatePDF']) -> middleware('auth:mahasiswa');
 
 
 //nilai
-Route::get('/nilai/select/{nidn}', [NilaiController::class, 'selectmk']);
-Route::get('/nilai/input/{kode_matakuliah}/{id_thn_akad}', [NilaiController::class, 'inputnilai']);
-Route::post('/nilai/save', [NilaiController::class, 'simpan'])-> middleware('auth:user,mahasiswa,dosen');;
+Route::get('/nilai/select/{nidn}', [NilaiController::class, 'selectmk'])-> middleware('auth:dosen');
+Route::get('/nilai/input/{kode_matakuliah}/{id_thn_akad}', [NilaiController::class, 'inputnilai'])-> middleware('auth:dosen');
+Route::post('/nilai/save', [NilaiController::class, 'simpan'])-> middleware('auth:ddosen');;
 
 
 //khs
-Route::get('/khs/view/{nim}/{thn}', [KhsController::class, 'coba']);
-Route::get('/khs/print/{nim}/{id_thn_akad}', [KhsController::class, 'generatePDF']);
+Route::get('/khs/view/{nim}/{thn}', [KhsController::class, 'coba'])-> middleware('auth:mahasiswa');
+Route::get('/khs/print/{nim}/{id_thn_akad}', [KhsController::class, 'generatePDF'])-> middleware('auth:mahasiswa');
 
 Route::get('/khs', function () {
     return view('khs-select');
-}) -> middleware('auth:user,mahasiswa,dosen');
+}) -> middleware('auth:mahasiswa');
 
 Route::get('/khs/view', function () {
     return view('khs-list');
-}) -> middleware('auth:user,mahasiswa,dosen');
+}) -> middleware('auth:mahasiswa');
 
 
 //profile
